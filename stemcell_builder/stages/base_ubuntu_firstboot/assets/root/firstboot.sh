1045552eebd3f0cef150e80d90dfad0b483d7845
Brian Cunnie <brian.cunnie@gmail.com>
2016-08-16 09:45:20 -0700

    firstboot.sh attempts to create ssh-keys for 60 seconds
    - fixes `Connection closed by remote host`
    - which is caused by no keys in `/etc/ssh`
    - which is caused by `dpkg-reconfigure` exiting prematurely with
      `gethostname: File name too long` before it can populate the keys
    - which in turn is caused by a very-long (65 character) hostname,
      e.g.  `vm-0eb85710-a7e5-4079-59d0-7fff6f3bf16d.c.blabbertabber.internal`
    - which in turn is updated within 5-10 seconds to a much shorter
      hostname (e.g. `ab12aa79-f2f6-4833-95ad-e86141c2db6c`), at which point
      `dpkg-reconfigure` will succeed.
    
    [fixes #1399]

--- stemcell_builder/stages/base_ubuntu_firstboot/assets/root/firstboot.sh
+++ stemcell_builder/stages/base_ubuntu_firstboot/assets/root/firstboot.sh
@@ -1,6 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
 
 rm -f /etc/ssh/ssh_host*key*
 
-dpkg-reconfigure -fnoninteractive -pcritical openssh-server
-dpkg-reconfigure -fnoninteractive sysstat
+RECONFIGURED=1
+COUNT=60
+
+while [ $COUNT -gt 0 ] && [ $RECONFIGURED != '0' ]; do
+	dpkg-reconfigure -fnoninteractive -pcritical openssh-server &&
+		dpkg-reconfigure -fnoninteractive sysstat
+	RECONFIGURED=$?
+	COUNT=$(( COUNT - 1 ))
+	sleep 1
+done
