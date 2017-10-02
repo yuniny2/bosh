e809eb76db27e86cc975dbda61481d5550400266
Kai Hofstetter <kai.hofstetter@sap.com>
2017-10-02 17:18:28 +0200

    Update director load test
    [#150958544](https://www.pivotaltracker.com/story/show/150958544)
    
    Signed-off-by: Tom Kiemes <tom.kiemes@sap.com>

--- src/spec/gocli/integration/cli_director_load_spec.rb
+++ src/spec/gocli/integration/cli_director_load_spec.rb
@@ -3,18 +3,28 @@ require_relative '../spec_helper'
 describe 'cli: director load test', type: :integration do
   with_reset_sandbox_before_each
 
+  before(:each) do
+    prepare_for_deploy
+
+    1.times do |i|
+      manifest_hash = Bosh::Spec::Deployments.simple_manifest
+      manifest_hash['name'] = "deployment_name_#{i.to_s}"
+      deploy_simple_manifest(manifest_hash: manifest_hash)
+    end
+  end
+
   it 'massively calls the director' do
     sandbox = Thread.current[:sandbox]
     threads = []
     failed = false
     failure_output = []
 
-    32000.times do
+    220.times do
       if !failed
         threads << Thread.new {
           if !failed
             Thread.current[:sandbox] = sandbox
-            output = bosh_runner.run('environment', failure_expected: true)
+            output = bosh_runner.run('deployments', failure_expected: true)
             if output.match('502')
               failure_output << output
               failed = true
