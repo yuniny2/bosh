8381c804db85e7a37d5466d2dee426311aa8efb4
Rob Day-Reynolds <rdayreynolds@pivotal.io>
2017-10-11 11:08:59 -0700

    WIP add global index and network id to record json

--- src/bosh-director/spec/unit/dns/dns_records_spec.rb
+++ src/bosh-director/spec/unit/dns/dns_records_spec.rb
@@ -16,9 +16,9 @@ module Bosh::Director
     describe '#to_json' do
       context 'with records' do
         before do
-          dns_records.add_record('uuid1', 1, 'group-name1', 'az1', 'net-name1', 'dep-name1', 'ip-addr1', 'bosh1.tld', 'fake-agent-uuid1')
-          dns_records.add_record('uuid2', 2, 'group-name2', 'az2', 'net-name2', 'dep-name2', 'ip-addr2', 'bosh1.tld', 'fake-agent-uuid1')
-          dns_records.add_record('uuid3', 3, 'group-name2', nil, 'net-name2', 'dep-name2', 'ip-addr3', 'bosh1.tld', 'fake-agent-uuid1')
+          dns_records.add_record('uuid1', 0, 1, 'group-name1', 'az1', 'net-name1', 0, 'dep-name1', 'ip-addr1', 'bosh1.tld', 'fake-agent-uuid1')
+          dns_records.add_record('uuid2', 1, 2, 'group-name2', 'az2', 'net-name2', 1, 'dep-name2', 'ip-addr2', 'bosh1.tld', 'fake-agent-uuid1')
+          dns_records.add_record('uuid3', 2, 3, 'group-name2', nil, 'net-name2', 1, 'dep-name2', 'ip-addr3', 'bosh1.tld', 'fake-agent-uuid1')
         end
 
         it 'returns json' do
@@ -29,17 +29,17 @@ module Bosh::Director
                  ['ip-addr3', 'uuid3.group-name2.net-name2.dep-name2.bosh1.tld']],
              'version' => 2,
              'record_keys' =>
-                 ['id', 'instance_group', 'group_ids', 'az', 'az_id', 'network', 'deployment', 'ip', 'domain', 'agent_id', 'instance_index'],
+                 ['id', 'global_index', 'instance_group', 'group_ids', 'az', 'az_id', 'network', 'network_id', 'deployment', 'ip', 'domain', 'agent_id', 'instance_index'],
              'record_infos' => [
-                 ['uuid1', 'group-name1', ['11'], 'az1', '3', 'net-name1', 'dep-name1', 'ip-addr1', 'bosh1.tld', 'fake-agent-uuid1', 1],
-                 ['uuid2', 'group-name2', ['12'], 'az2', '7', 'net-name2', 'dep-name2', 'ip-addr2', 'bosh1.tld', 'fake-agent-uuid1', 2],
-                 ['uuid3', 'group-name2', ['12'], nil, nil, 'net-name2', 'dep-name2', 'ip-addr3', 'bosh1.tld', 'fake-agent-uuid1', 3]],
+                 ['uuid1', '0', 'group-name1', ['11'], 'az1', '3', 'net-name1', '0', 'dep-name1', 'ip-addr1', 'bosh1.tld', 'fake-agent-uuid1', 1],
+                 ['uuid2', '1', 'group-name2', ['12'], 'az2', '7', 'net-name2', '1', 'dep-name2', 'ip-addr2', 'bosh1.tld', 'fake-agent-uuid1', 2],
+                 ['uuid3', '2', 'group-name2', ['12'],   nil, nil, 'net-name2', '1', 'dep-name2', 'ip-addr3', 'bosh1.tld', 'fake-agent-uuid1', 3]],
           }
           expect(JSON.parse(dns_records.to_json)).to eq(expected_records)
         end
 
         it 'returns the shasum' do
-          expect(dns_records.shasum).to eq('bb76182dcd83143a3627c35622626b3575e62481')
+          expect(dns_records.shasum).to eq('db994ee9617fe234faec41d615cf3c47be047576')
         end
 
         context 'when index records are enabled' do
@@ -56,11 +56,11 @@ module Bosh::Director
                     ['ip-addr3', '3.group-name2.net-name2.dep-name2.bosh1.tld']],
                 'version' => 2,
                 'record_keys' =>
-                    ['id', 'instance_group', 'group_ids', 'az', 'az_id', 'network', 'deployment', 'ip', 'domain', 'agent_id', 'instance_index'],
+                    ['id', 'global_index', 'instance_group', 'group_ids', 'az', 'az_id', 'network', 'network_id', 'deployment', 'ip', 'domain', 'agent_id', 'instance_index'],
                 'record_infos' => [
-                    ['uuid1', 'group-name1', ['11'], 'az1', '3', 'net-name1', 'dep-name1', 'ip-addr1', 'bosh1.tld', 'fake-agent-uuid1', 1],
-                    ['uuid2', 'group-name2', ['12'], 'az2', '7', 'net-name2', 'dep-name2', 'ip-addr2', 'bosh1.tld', 'fake-agent-uuid1', 2],
-                    ['uuid3', 'group-name2', ['12'], nil, nil, 'net-name2', 'dep-name2', 'ip-addr3', 'bosh1.tld', 'fake-agent-uuid1', 3]],
+                    ['uuid1', '0', 'group-name1', ['11'], 'az1', '3', 'net-name1', '0', 'dep-name1', 'ip-addr1', 'bosh1.tld', 'fake-agent-uuid1', 1],
+                    ['uuid2', '1', 'group-name2', ['12'], 'az2', '7', 'net-name2', '1', 'dep-name2', 'ip-addr2', 'bosh1.tld', 'fake-agent-uuid1', 2],
+                    ['uuid3', '2', 'group-name2', ['12'],   nil, nil, 'net-name2', '1', 'dep-name2', 'ip-addr3', 'bosh1.tld', 'fake-agent-uuid1', 3]],
             }
             expect(JSON.parse(dns_records.to_json)).to eq(expected_records)
           end
@@ -68,7 +68,7 @@ module Bosh::Director
 
         context 'canonicalization' do
           before do
-            dns_records.add_record('uuid4', 4, 'group_name3', 'az3', 'net_name3', 'dep_name3', 'ip-addr4', 'bosh3.tld', 'fake-agent-uuid3')
+            dns_records.add_record('uuid4', 3, 4, 'group_name3', 'az3', 'net_name3', 2, 'dep_name3', 'ip-addr4', 'bosh3.tld', 'fake-agent-uuid3')
           end
 
           it 'canonicalizes the network name, deployment name and instance group name' do
@@ -80,12 +80,12 @@ module Bosh::Director
                 ['ip-addr4', 'uuid4.group-name3.net-name3.dep-name3.bosh3.tld']],
               'version' => 2,
               'record_keys' =>
-                ['id', 'instance_group', 'group_ids', 'az', 'az_id', 'network', 'deployment', 'ip', 'domain', 'agent_id', 'instance_index'],
+                ['id', 'global_index', 'instance_group', 'group_ids', 'az', 'az_id', 'network', 'network_id', 'deployment', 'ip', 'domain', 'agent_id', 'instance_index'],
               'record_infos' => [
-                ['uuid1', 'group-name1', ['11'], 'az1', '3', 'net-name1', 'dep-name1', 'ip-addr1', 'bosh1.tld', 'fake-agent-uuid1', 1],
-                ['uuid2', 'group-name2', ['12'], 'az2', '7', 'net-name2', 'dep-name2', 'ip-addr2', 'bosh1.tld', 'fake-agent-uuid1', 2],
-                ['uuid3', 'group-name2', ['12'], nil, nil, 'net-name2', 'dep-name2', 'ip-addr3', 'bosh1.tld', 'fake-agent-uuid1', 3],
-                ['uuid4', 'group-name3', ['13'], 'az3', '11', 'net-name3', 'dep-name3', 'ip-addr4', 'bosh3.tld', 'fake-agent-uuid3', 4]]
+                ['uuid1', '0', 'group-name1', ['11'], 'az1',  '3', 'net-name1', '0', 'dep-name1', 'ip-addr1', 'bosh1.tld', 'fake-agent-uuid1', 1],
+                ['uuid2', '1', 'group-name2', ['12'], 'az2',  '7', 'net-name2', '1', 'dep-name2', 'ip-addr2', 'bosh1.tld', 'fake-agent-uuid1', 2],
+                ['uuid3', '2', 'group-name2', ['12'],   nil,  nil, 'net-name2', '1', 'dep-name2', 'ip-addr3', 'bosh1.tld', 'fake-agent-uuid1', 3],
+                ['uuid4', '3', 'group-name3', ['13'], 'az3', '11', 'net-name3', '2', 'dep-name3', 'ip-addr4', 'bosh3.tld', 'fake-agent-uuid3', 4]]
             }
             expect(JSON.parse(dns_records.to_json)).to eq(expected_records)
           end
@@ -94,8 +94,8 @@ module Bosh::Director
 
       context 'when have 0 records' do
         it 'returns empty json' do
-          expect(dns_records.to_json).to eq('{"records":[],"version":2,"record_keys":["id","instance_group","group_ids","az","az_id","network","deployment","ip","domain","agent_id","instance_index"],"record_infos":[]}')
-          expect(dns_records.to_json).to eq('{"records":[],"version":2,"record_keys":["id","instance_group","group_ids","az","az_id","network","deployment","ip","domain","agent_id","instance_index"],"record_infos":[]}')
+          expect(dns_records.to_json).to eq('{"records":[],"version":2,"record_keys":["id","global_index","instance_group","group_ids","az","az_id","network","network_id","deployment","ip","domain","agent_id","instance_index"],"record_infos":[]}')
+          expect(dns_records.to_json).to eq('{"records":[],"version":2,"record_keys":["id","global_index","instance_group","group_ids","az","az_id","network","network_id","deployment","ip","domain","agent_id","instance_index"],"record_infos":[]}')
         end
       end
     end
